18e9e99927f7ab2d2d5181e4180a280068061022
Raghav <dinesh24111976@gmail.com>
2026-07-27 19:01:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.9 MB (40.97%)

---
+++ 2058-concatenation-of-array/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+      vector<int> v = nums;
+        v.insert(v.end(), nums.begin(), nums.end());
+        return v;
+    }
+};
