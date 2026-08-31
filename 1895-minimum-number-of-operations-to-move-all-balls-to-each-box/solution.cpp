137f330f6e3b2a87af512aed3d877f045674e7a4
Raghav <dinesh24111976@gmail.com>
2026-08-31 15:06:09 +0000

    Sync LeetCode submission Runtime - 119 ms (15.72%), Memory - 12.3 MB (52.85%)

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n=boxes.size();
+        vector<int>ans(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (boxes[j] == '1')
+                    ans[i] =ans[i]+(abs(i - j));
+            }
+        }
+        return ans;
+    }
+};
